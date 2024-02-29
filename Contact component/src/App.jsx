3034536226f7d32f8c9436df2d65b7fe52204501
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-02-29 21:04:55 +0400

    add contact component

---
+++ Contact component/src/App.jsx
@@ -0,0 +1,34 @@
+import Contact from "./components/Contact";
+import photo from "./images/download.jpg";
+function App() {
+  return (
+    <div className="contacts">
+      <Contact
+        img={photo}
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img={photo}
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img={photo}
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        img={photo}
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
+    </div>
+  );
+}
+
+export default App;
